0ed53c3d43156103c80baf6296b91c0207d44bf2
szlam <lam_shao_zhe@defence.gov.sg>
2022-09-14 07:10:59 +0800

    Commit 4 - Feature to generate a random 9x9 board added. Click event handlers for menu updated.

--- handler.js
+++ handler.js
@@ -1,22 +1,78 @@
 "use strict";
 
+////=============== Logic for Clicks on Menu ===============
+//Summary: Click Eventlisteners and SwitchBoard for Menu
+//Change Logs:
+//Commit 3: Eventlistener and SwitchBoard for 5 initial buttons created
+//Commit 4:
+//Notes: each switch case should only trigger one function for consistency and neatness
 document.querySelector("#buttons").addEventListener("click", function (e) {
-  console.log(e.target.id);
+  //e.preventDefault();
   switch (e.target.id) {
     case "generateBoard":
-      console.log("1");
+      fGenerateRandomBoard();
       break;
     case "fillBoard":
-      console.log("2");
+      fGetFromBoard();
       break;
     case "checkBoard":
-      console.log("3");
+      fCheckBoard();
       break;
     case "solveBoard":
-      console.log("4");
+      fSolveBoard();
       break;
     case "showHints":
-      console.log("5");
+      fShowHints();
       break;
   }
 });
+
+////=============== Higher Order Functions for Click Logic ===============
+//Change Logs:
+//Commit 4: Individual button functions created. Button 1 and 4 functional.
+
+let getDisplay = document.querySelector("#display");
+
+//Upon click, generate a random & valid board on html
+function fGenerateRandomBoard() {
+  getDisplay.textContent +=
+    "Generate Board was clicked => A random puzzle had been generated. ";
+  initialiseArrs();
+  boardGenerator(n, depth, true);
+  populateInitialValues();
+  writeToBoard(initialArr);
+}
+
+function fGetFromBoard() {
+  getDisplay.textContent += "Your inputs had been recorded. ";
+  initialiseArrs();
+  boardGenerator(n, depth, false);
+}
+
+function fCheckBoard() {
+  getDisplay.textContent += "Button still in development";
+}
+
+function fSolveBoard() {
+  getDisplay.textContent += generateSolutionsforHTML();
+  writeToBoard(solnArr);
+}
+
+function fShowHints() {
+  getDisplay.textContent += errArr;
+}
+
+//Takes a nxn matrix for disp (HTML limited to 9x9)
+function writeToBoard(arr) {
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[i][j] === 0 || arr[i][j] === -1) {
+        document.querySelectorAll(".gridElement")[count].value = null;
+      } else {
+        document.querySelectorAll(".gridElement")[count].value = arr[i][j];
+      }
+      count++;
+    }
+  }
+}
--- script.js
+++ script.js
@@ -1,34 +1,18 @@
 "use strict";
 
-//so for DOM only need to play with initialArr and n
-
-const initialArr = [
-  [0, 6, 0, 0, 0, 0, 0, 1, 0],
-  [7, 0, 1, 0, 0, 0, 0, 0, 4],
-  [0, 0, 3, 9, 0, 0, 0, 0, 0],
-  [6, 0, 9, 0, 0, 0, 0, 0, 5],
-  [0, 5, 0, 0, 0, 9, 7, 2, 0],
-  [0, 7, 0, 2, 0, 6, 0, 0, 0],
-  [0, 0, 6, 0, 3, 0, 0, 0, 0],
-  [9, 0, 0, 7, 0, 0, 5, 3, 0],
-  [0, 0, 0, 0, 9, 0, 1, 0, 0],
-];
-
-let solnArr = [],
+let initialArr = [],
+  solnArr = [],
   errArr = [],
-  n = 9;
+  n = 9,
+  depth = 2;
 
-initialiseArrs(n); //create 9x9 solution array which records answer, and 9x9x9 error array to check valid moves
-populateInitialValues(); //Mainly to push valid answers for each grid onto errArr
-console.log(errArr);
-console.log(solnArr);
-
-if (autoSolver()) {
-  console.log(solnArr);
-} else {
-  console.log("no solution exists");
+function generateSolutionsforHTML() {
+  if (autoSolver()) {
+    return "Solve Puzzle was clicked => Solutions generated.";
+  } else {
+    return "Solve Puzzle was clicked => However, no solution exists";
+  }
 }
-
 function autoSolver(row = 0, col = 0) {
   //write base case first to stop recussion => in this case condition where board is solved
   if (checkComplete(row, col)) {
@@ -63,7 +47,6 @@ function autoSolver(row = 0, col = 0) {
   //console.log("false is returned");
   return false;
 }
-
 function checkComplete(row, col) {
   //if the current row is index 8 and col is index 9 means 8 : 8 had been solved
   if (row === solnArr[0].length - 1 && col === solnArr.length) {
@@ -72,13 +55,103 @@ function checkComplete(row, col) {
   return false;
 }
 
-function initialiseArrs(n) {
-  solnArr = new Array(n).fill(new Array(n).fill(-1));
-  errArr = new Array(n).fill(new Array(n).fill([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+//=============== Boards Generator Functions below ===============
+//Summary: to create 3 arrays that are ready for solver to take in and solve for solutions
+//Change Logs:
+//Commit 1: Sets up the 3 array for a fixed array defined within JS.
+//Commit 4: Created boardGenerator function that allows for generation of 9x9 boards.
+//Note: Only for 9x9 board for now. Code is NOT READY for nxn where n is any number and NOT READY for use in 3D (nxnxn) boards. though some features for n and depths are considered below.
+//Note: by default uses a pre-generated board
+
+//Returns 3 initiallised arrays for errArr (nxnxn), solnArr (nxn) and initialArr. Input the gridlength and dimension of selected board
+function initialiseArrs(n = 9, depth = 2) {
+  (n = parseInt(n)), (depth = parseInt(depth));
+
+  //generate all soln on errArr depending on n length first
+  let tempArr = new Array(n);
+  for (let i = 0; i < n; i++) {
+    tempArr[i] = i + 1;
+  }
+
+  //Generates dimensions dimensions next
+  if (depth === 2) {
+    solnArr = new Array(n).fill(new Array(n).fill(-1));
+    initialArr = new Array(n).fill(new Array(n).fill(0));
+    errArr = new Array(n).fill(new Array(n).fill(-1));
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        errArr[i][j] = tempArr;
+      }
+    }
+  }
+  if (depth === 3) {
+    solnArr = new Array(n).fill(new Array(n).fill(new Array(n).fill(-1)));
+    initialArr = new Array(n).fill(new Array(n).fill(new Array(n).fill(0)));
+    errArr = new Array(n).fill(new Array(n).fill(new Array(n).fill(-1)));
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        for (let k = 0; k < n; k++) {
+          errArr[i][j][k] = tempArr;
+        }
+      }
+    }
+  }
+
+  //making all array elements unique (not references of each other)
   solnArr = JSON.parse(JSON.stringify(solnArr));
+  initialArr = JSON.parse(JSON.stringify(initialArr));
   errArr = JSON.parse(JSON.stringify(errArr));
 }
 
+//Either generate a 9x9 board or use a default 9x9 board depending on user selection
+function boardGenerator(n = 9, depth = 2, isGenerateTrue = false) {
+  let count = 0;
+  if (isGenerateTrue) {
+    while (count < n * depth) {
+      let num = Math.ceil(Math.random() * n);
+      let x = Math.floor(Math.random() * n);
+      let y = Math.floor(Math.random() * n);
+      if (depth === 3) {
+        let z = Math.floor(Math.random() * n);
+      }
+      if (isNotConstrained(num, x, y, initialArr) && depth === 2) {
+        initialArr[x][y] = num;
+        count++;
+      }
+      //   if (isNotConstrained(num, x, y, z, initialArr) && depth === 3) {
+      //     initialArr[x][y][z] = num;
+      //     count++;
+      //   }
+    }
+  } else {
+    // initialArr = [
+    //   [0, 6, 0, 0, 0, 0, 0, 1, 0],
+    //   [7, 0, 1, 0, 0, 0, 0, 0, 4],
+    //   [0, 0, 3, 9, 0, 0, 0, 0, 0],
+    //   [6, 0, 9, 0, 0, 0, 0, 0, 5],
+    //   [0, 5, 0, 0, 0, 9, 7, 2, 0],
+    //   [0, 7, 0, 2, 0, 6, 0, 0, 0],
+    //   [0, 0, 6, 0, 3, 0, 0, 0, 0],
+    //   [9, 0, 0, 7, 0, 0, 5, 3, 0],
+    //   [0, 0, 0, 0, 9, 0, 1, 0, 0],
+    // ];
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        console.log(document.querySelectorAll(".gridElement").value);
+        if (document.querySelectorAll(".gridElement").value == undefined) {
+          initialArr[i][j] = 0;
+        } else {
+          initialArr[i][j] = document.querySelectorAll(".gridElement").value;
+        }
+        count++;
+      }
+    }
+    console.log(initialArr);
+  }
+}
+
+//Creating a working board: SolnArr, and error board: ErrArr for working of solutions in solver, and errArr to generate hints later
 function populateInitialValues() {
   //limiting all solvable grids to be contrained - i.e will never conflict with solve/ default puzzle input in the same row, col, box
   for (let i = 0; i < initialArr.length; i++) {
@@ -100,6 +173,7 @@ function populateInitialValues() {
   }
 }
 
+//all other remove functions below are lower order functions called solely by populateInitialValues
 function removeElement(num, row, col) {
   removeElementInCol(num - 1, row, col);
   removeElementInRow(num - 1, row, col);
@@ -129,24 +203,29 @@ function removeElementInBox(num, row, col) {
   }
 }
 
-function isNotConstrained(num, row, col) {
-  //returns true if not contrained, i.e valid move
+//=============== Constrains Checker Functions below ===============
+//Change Logs:
+//Commit 1: Takes in num, row, col for any nxn boards to check if a move is valid. isNotConstrained returns true if move is valid
+//Commit 4: Able to take in array when reference is specified -> For use in board generation
+//Note: Code is ready for nxn where n is any number, but NOT READY for use in 3D (nxnxn) boards
+//Note: All row and col arguments for functions are array index (i.e starts from 0)
+
+//returns true if not contrained, i.e valid move. All other functions below are lower order functions called solely by isNotConstrained
+function isNotConstrained(num, row, col, arr = solnArr) {
   if (
-    isRowNotConstrained(num, row, col) &&
-    isColNotConstrained(num, row, col) &&
-    isBoxNotConstrained(num, row, col)
+    isRowNotConstrained(num, row, col, arr) &&
+    isColNotConstrained(num, row, col, arr) &&
+    isBoxNotConstrained(num, row, col, arr)
   ) {
-    //console.log("true is returned");
     return true;
   } else {
-    //console.log("false is returned");
     return false;
   }
 }
 
-function isRowNotConstrained(num, row, col) {
-  const solnArrR = solnArr[row];
-  let rFinder = solnArrR.find((element) => element === num); //returns undefined if no number found
+function isRowNotConstrained(num, row, col, arr) {
+  const arrR = arr[row];
+  let rFinder = arrR.find((element) => element === num);
   //console.log(`R returning: ${solnArrR}, rFinder = ${rFinder}, testing ${num}`);
   if (rFinder === undefined) {
     return true;
@@ -155,13 +234,13 @@ function isRowNotConstrained(num, row, col) {
   }
 }
 
-function isColNotConstrained(num, row, col) {
-  const solnArrC = [];
+function isColNotConstrained(num, row, col, arr) {
+  const arrC = [];
   let cFinder;
-  for (let i = 0; i < solnArr.length; i++) {
-    solnArrC.push(solnArr[i][col]);
+  for (let i = 0; i < arr.length; i++) {
+    arrC.push(arr[i][col]);
   }
-  cFinder = solnArrC.find((element) => element === num);
+  cFinder = arrC.find((element) => element === num);
   //console.log(`C returning: ${solnArrC}, cFinder = ${cFinder}, testing ${num}`);
   if (cFinder === undefined) {
     return true;
@@ -170,17 +249,17 @@ function isColNotConstrained(num, row, col) {
   }
 }
 
-function isBoxNotConstrained(num, row, col) {
-  const solnArrB = [];
+function isBoxNotConstrained(num, row, col, arr) {
+  const arrB = [];
   let bFinder, localRow, localCol;
   localRow = row - (row % 3);
   localCol = col - (col % 3);
   for (let i = localRow; i < localRow + 3; i++) {
     for (let j = localCol; j < localCol + 3; j++) {
-      solnArrB.push(solnArr[i][j]);
+      arrB.push(arr[i][j]);
     }
   }
-  bFinder = solnArrB.find((element) => element === num);
+  bFinder = arrB.find((element) => element === num);
   //console.log(`B returning: ${solnArrB}, bFinder = ${bFinder}, testing ${num}`);
   if (bFinder === undefined) {
     return true;
