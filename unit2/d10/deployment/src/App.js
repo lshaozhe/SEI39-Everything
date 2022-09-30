75bac00ec87d80262e1f7162705f268088ffa82b
lshaozhe <104511400+lshaozhe@users.noreply.github.com>
2022-09-30 11:04:18 +0800

    slight amendments to u2d10

--- unit2/d10/deployment/src/App.js
+++ unit2/d10/deployment/src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
-import FirstApp from "./components/FirstApp";
-import SecontApp from "./components/SecontApp";
+import React, { Suspense } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./pages/Main";
+// import PageOne from "./pages/PageOne";
+// import PageTwo from "./pages/PageTwo";
+// import PageThree from "./pages/PageThree";
+// import Details from "./pages/Details";
+import NavBar from "./components/NavBar";
+
+const PageOne = React.lazy(() => import("./pages/PageOne"));
+const PageTwo = React.lazy(() => import("./pages/PageTwo"));
+const PageThree = React.lazy(() => import("./pages/PageThree"));
+const Details = React.lazy(() => import("./pages/Details"));
 
 function App() {
   return (
     <div className="container">
-      <div className="row">
-        <FirstApp />
-      </div>
-      <hr />
-      <div className="row">
-        <SecontApp />
-      </div>
+      <Suspense fallback={<p>Loading ...</p>}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/page-one" />} />
+          <Route path="/page-one" element={<PageOne />} />
+          <Route path="/page-two" element={<PageTwo />} />
+          <Route path="/page-three" element={<PageThree />} />
+          <Route path="/page-one/:item" element={<Details />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
