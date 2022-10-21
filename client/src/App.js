73d1ea4d4663026a727ec083a85a664416f47eaf
zappled <107865571+zappled@users.noreply.github.com>
2022-10-21 17:35:40 +0800

    Edited homepage, combined with footer (#16)
    * Updated navbar
    
    * Edited homepage, combined with footer
    
    * Added comments for homepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,14 @@
 
 import "./App.css";
 import "@coreui/coreui/dist/css/coreui.min.css";
-import Navbar from "./common/Navbar";
-import Footer from "./common/Footer";
 import Homepage from "./pages/Homepage";
 
 function App() {
-	return (
-		<>
-			<Homepage />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Homepage />
+    </>
+  );
 }
 
 export default App;
