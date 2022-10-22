303e114f6206287b256dba8cdd27c4a68542054d
zappled <j0el_93@hotmail.com>
2022-10-22 21:47:59 +0800

    Added banner with embed video and carousel

--- client/src/pages/mdw/StudentStories.js
+++ client/src/pages/mdw/StudentStories.js
@@ -7,6 +7,10 @@ import eventsIconFour from "../../assets/mdw_pages/stories_02d.jpg";
 import eventsIconFive from "../../assets/mdw_pages/stories_02e.jpg";
 import eventsIconSix from "../../assets/mdw_pages/stories_02f.jpg";
 import eventsIconSeven from "../../assets/mdw_pages/stories_02g.jpg";
+import { CCarousel } from "@coreui/react";
+import { CCarouselItem } from "@coreui/react";
+import { CCarouselCaption } from "@coreui/react";
+import { CCallout } from "@coreui/react";
 
 const StudentStories = () => {
   return (
@@ -14,6 +18,54 @@ const StudentStories = () => {
       <div className="page_container">
         <div className="page_header">
           <span>For Students / Student Stories</span>
+        </div>
+        <div className={styles.banner}>
+          <div className={styles.banner_video}>
+            <iframe src="https://www.youtube.com/embed/xsuNrqkzNqI"></iframe>
+          </div>
+          <div className={styles.banner_caption}>
+            <CCarousel
+              indicators
+              className="banner_container"
+              transition="crossfade"
+            >
+              <CCarouselItem>
+                <div className={styles.carousel_item}>
+                  <h2>
+                    “I feel that I’m finally on the right path to giving my
+                    daughter a good future.”
+                  </h2>
+                  <p>
+                    <span>🇵🇭</span> @YolandaBermas
+                  </p>
+                </div>
+              </CCarouselItem>
+              {/* second carousel image */}
+              <CCarouselItem>
+                <div className={styles.carousel_item}>
+                  <h2>
+                    “Aidha changed my life. It gave me the chance to reach my
+                    dream, not only for myself but also for my family.”
+                  </h2>
+                  <p>
+                    <span>🇵🇭</span>@Gleia
+                  </p>
+                </div>
+              </CCarouselItem>
+              {/* third carousel image */}
+              <CCarouselItem>
+                <div className={styles.carousel_item}>
+                  <h2>
+                    Thanks to Aidha, I now know how to manage my assets better
+                    and make them work for me.
+                  </h2>
+                  <p>
+                    <span>🇵🇭</span>@ArnolitaCruz
+                  </p>
+                </div>
+              </CCarouselItem>
+            </CCarousel>
+          </div>
         </div>
         <div className={styles.events_icons_container}>
           <div className={styles.events_box} style={{ marginTop: "3vh" }}>
