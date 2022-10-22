3cde605c78d249c8d301a643830fbe102d87f931
ruyi <tan_ruyi@hotmail.com>
2022-10-22 14:27:00 +0800

    route added for volunteers - get involved

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import "@coreui/coreui/dist/css/coreui.min.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import CourseInfo from "./pages/mdw/CourseInfo";
+import GetInvolved from "./pages/volunteer/GetInvolved";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/students/courses/financial_education" element={<CourseInfo />} />
+                <Route path="/volunteers" element={<GetInvolved />} />
 			</Routes>
 		</>
 	);
