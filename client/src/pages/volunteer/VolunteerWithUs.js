1e9804f4d5678f9aad074c76fba828d591e02c78
zappled <107865571+zappled@users.noreply.github.com>
2022-10-23 20:18:15 +0800

    Updates to 'volunteer with us' page (#29)

--- client/src/pages/volunteer/VolunteerWithUs.js
+++ client/src/pages/volunteer/VolunteerWithUs.js
@@ -157,39 +157,36 @@ const VolunteerWithUs = () => {
               <img src={bannerTwo} alt="" />
             </div>
             {/* first carousel item content body */}
-              <span>Mentor (Module 1, 2 or 3)</span>
+            <span>Mentor (Module 1, 2 or 3)</span>
+          </div>
+          <div className={styles.content_image}>
+            <img src={bannerTwo} alt="" />
+          </div>
+          <div className={styles.content_button}>
+            <div>
+              <span>Time Commitment</span>
+              <br />
+              <p>• Minimum period of 6 months</p>
+              <p style={{ marginLeft: "7.5vw" }}>• 1.5 hours every Sunday OR</p>
+              <p style={{ marginLeft: "7.5vw" }}>• 3 hours every 2 Sundays</p>
             </div>
-            <div className={styles.content_image}>
-              <img src={bannerTwo} alt="" />
-            </div>
-            <div className={styles.content_button}>
-              <div>
-                <span>Time Commitment</span>
-                <br />
-                <p>• Minimum period of 6 months</p>
-                <p style={{ marginLeft: "7.5vw" }}>
-                  • 1.5 hours every Sunday OR
-                </p>
-                <p style={{ marginLeft: "7.5vw" }}>• 3 hours every 2 Sundays</p>
-              </div>
-              <span>Location</span>
-              <br />
-              <p>• Online on Zoom OR</p>
-              <p>• FAST Hub @ 3 Chin Cheng Avenue, S429401</p>
-              <span>Requirements</span>
-              <br />
-              <p>• Minimum 3 years of work experience in any field</p>
-              <span>Responsibilities</span>
-              <br />
-              <p>
-                • Adopt a class and take the students on a learning journey that
-                will build their confidence in themselves and their future
-              </p>
-              {/* first carousel buttons */}
-              <div className={styles.roles_button_container}>
-                <div className={styles.roles_button}>More Info</div>
-                <div className={styles.roles_button}>Apply Now</div>
-              </div>
+            <span>Location</span>
+            <br />
+            <p>• Online on Zoom OR</p>
+            <p>• FAST Hub @ 3 Chin Cheng Avenue, S429401</p>
+            <span>Requirements</span>
+            <br />
+            <p>• Minimum 3 years of work experience in any field</p>
+            <span>Responsibilities</span>
+            <br />
+            <p>
+              • Adopt a class and take the students on a learning journey that
+              will build their confidence in themselves and their future
+            </p>
+            {/* first carousel buttons */}
+            <div className={styles.roles_button_container}>
+              <div className={styles.roles_button}>More Info</div>
+              <div className={styles.roles_button}>Apply Now</div>
             </div>
           </div>
           {/* second carousel item */}
@@ -203,42 +200,38 @@ const VolunteerWithUs = () => {
               <img src={bannerThree} alt="" />
             </div>
             {/* second carousel item content body */}
-              <span>Mentor (Computer Classes)</span>
-            </div>
-            <div className={styles.content_image}>
-              <img src={bannerThree} alt="" />
-            </div>
-            <div className={styles.content_button}>
-              <div>
-                <span>Time Commitment</span>
-                <br />
-                <p>• Minimum period of 6 months</p>
-                <p style={{ marginLeft: "7.5vw" }}>
-                  • 1.5 hours every Sunday OR
-                </p>
-                <p style={{ marginLeft: "7.5vw" }}>• 3 hours every 2 Sundays</p>
-              </div>
-              <span>Location</span>
+            <span>Mentor (Computer Classes)</span>
+          </div>
+          <div className={styles.content_image}>
+            <img src={bannerThree} alt="" />
+          </div>
+          <div className={styles.content_button}>
+            <div>
+              <span>Time Commitment</span>
               <br />
-              <p>• Online on Zoom OR</p>
-              <p>• FAST Hub @ 3 Chin Cheng Avenue, S429401</p>
-              <span>Requirements</span>
-              <br />
-              <p>• Minimum 1 year of work experience in any field</p>
-              <p>• Good Computer Skills</p>
-              <span>Responsibilities</span>
-              <br />
-              <p>
-                • Adopt a class and help students improve practical IT skills on
-                computer and phone
-              </p>
-              {/* second carousel item buttons */}
-              <div className={styles.roles_button_container}>
-                <div className={styles.roles_button}>More Info</div>
-                <div className={styles.roles_button}>Apply Now</div>
-              </div>
+              <p>• Minimum period of 6 months</p>
+              <p style={{ marginLeft: "7.5vw" }}>• 1.5 hours every Sunday OR</p>
+              <p style={{ marginLeft: "7.5vw" }}>• 3 hours every 2 Sundays</p>
             </div>
-            {/* button_label to become navigation link */}
+            <span>Location</span>
+            <br />
+            <p>• Online on Zoom OR</p>
+            <p>• FAST Hub @ 3 Chin Cheng Avenue, S429401</p>
+            <span>Requirements</span>
+            <br />
+            <p>• Minimum 1 year of work experience in any field</p>
+            <p>• Good Computer Skills</p>
+            <span>Responsibilities</span>
+            <br />
+            <p>
+              • Adopt a class and help students improve practical IT skills on
+              computer and phone
+            </p>
+            {/* second carousel item buttons */}
+            <div className={styles.roles_button_container}>
+              <div className={styles.roles_button}>More Info</div>
+              <div className={styles.roles_button}>Apply Now</div>
+            </div>
           </div>
           {/* third carousel item */}
           <div className={styles.content}>
