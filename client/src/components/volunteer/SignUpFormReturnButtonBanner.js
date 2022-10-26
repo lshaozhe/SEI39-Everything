c0b1e133588de61be145068492f2e4b49ac9ba7e
zappled <j0el_93@hotmail.com>
2022-10-26 14:04:57 +0800

    Updated font colour at SignUpFormReturnButtonBanner component

--- client/src/components/volunteer/SignUpFormReturnButtonBanner.js
+++ client/src/components/volunteer/SignUpFormReturnButtonBanner.js
@@ -6,14 +6,18 @@ import styles from "./SignUpFormReturnButtonBanner.module.css";
 import leftArrow from "../../assets/svg_icons/leftarrow.svg";
 
 const SignUpFormReturnButtonBanner = () => {
-	return (
-		<div className={styles.container}>
-			<Link to="/volunteers/volunteer_with_us" className={styles.navigateBack}>
-				<img className={styles.leftArrowImg} src={leftArrow} alt="left-arrow" />
-				<h6 className={styles.navigationText}>Back to Volunteer page</h6>
-			</Link>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <Link
+        to="/volunteers/volunteer_with_us"
+        className={styles.navigateBack}
+        style={{ textDecoration: "none", color: "#f68121" }}
+      >
+        <img className={styles.leftArrowImg} src={leftArrow} alt="left-arrow" />
+        <h6 className={styles.navigationText}>Back to Volunteer page</h6>
+      </Link>
+    </div>
+  );
 };
 
 export default SignUpFormReturnButtonBanner;
