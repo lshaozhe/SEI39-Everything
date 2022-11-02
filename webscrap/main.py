49867c4f95924669ce904c4e10a28c49a222a84b
szlam <lam_shao_zhe@defence.gov.sg>
2022-11-02 17:21:21 +0800

    added scraper function for product details

--- webscrap/main.py
+++ webscrap/main.py
@@ -57,6 +57,34 @@ def scrap_product_links(url):
     print('scrapper returned {} products from {}'.format(len(response), url))
 
 
+def scrap_product_details(url='https://www.fairprice.com.sg/product/fairprice-gold-3-ply-bathtroom-tissues-10-rolls-13095920'):
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    product_name = soup.find('span', attrs={'class': 'sc-1bsd7ul-1 djlKtC'}).text.strip()
+    product_price = soup.find('span', attrs={'class': 'sc-1bsd7ul-1 sc-13n2dsm-5 kxEbZl deQJPo'}).text.strip()
+    product_short_description = soup.find('span', attrs={'class': 'sc-1bsd7ul-1 LLmwF'}).text.strip()
+    product_brand = soup.find('a', attrs={'class': 'sc-13n2dsm-1 jLtMNk'}).text.strip()
+    product_main_image = re.search('\((.*?)\)', str(soup.find('div', attrs={'class': 'sc-vhxzaf-0 cTfRiy'}))).group(1)
+    product_more_images = soup.find_all('img', attrs={'class': 'sc-10zw1uf-11 gyQcYf'})
+    uncleaned_product_long_description = soup.find_all('div', attrs={'class': 'sc-3zvnd-0 hOgsAE'})
+
+    print(soup.prettify())
+    print(product_name)
+    print(product_price)
+    print(product_short_description)
+    print(product_brand)
+    print(product_main_image)
+    for item in product_more_images:
+        print(item['src'])
+
+    product_long_description = []
+    for item in uncleaned_product_long_description:
+        product_long_description.append(re.findall('(?<=>)[a-zA-Z0-9\ &;!,/]+(?=<)', str(item)))
+
+    print(product_long_description)
+
+
 def selenium_scroll_and_return_html(url):
     # SET SCROLL PAUSE TIME FIRST
     selenium_scroll_pause_time = 0.5
@@ -92,4 +120,5 @@ def start_scrap():
 
 
 if __name__ == '__main__':
-    start_scrap()
+    # start_scrap()
+    scrap_product_details('https://www.fairprice.com.sg/product/magiclean-bathroom-stain-mold-remover-400ml-12329292')
