353cb81cc770e20b693ae4bba20cf21fffa4e4f1
szlam <lam_shao_zhe@defence.gov.sg>
2022-11-03 17:49:32 +0800

    updated alittle

--- webscrap/main.py
+++ webscrap/main.py
@@ -71,30 +71,50 @@ def scrap_product_links(url, selenium_driver):
 
 def scrap_product_details(url='https://www.fairprice.com.sg/product/fairprice-gold-3-ply-bathtroom-tissues-10-rolls-13095920'):
     page = requests.get(url)
+    product = dict()
 
     soup = BeautifulSoup(page.content, "html.parser")
     product_name = soup.find('span', attrs={'class': 'sc-1bsd7ul-1 djlKtC'}).text.strip()
     product_price = soup.find('span', attrs={'class': 'sc-1bsd7ul-1 sc-13n2dsm-5 kxEbZl deQJPo'}).text.strip()
-    product_short_description = soup.find('span', attrs={'class': 'sc-1bsd7ul-1 LLmwF'}).text.strip()
+    product_description = soup.find('span', attrs={'class': 'sc-1bsd7ul-1 LLmwF'}).text.strip()
     product_brand = soup.find('a', attrs={'class': 'sc-13n2dsm-1 jLtMNk'}).text.strip()
-    product_main_image = re.search('\((.*?)\)', str(soup.find('div', attrs={'class': 'sc-vhxzaf-0 cTfRiy'}))).group(1)
-    product_more_images = soup.find_all('img', attrs={'class': 'sc-10zw1uf-11 gyQcYf'})
-    uncleaned_product_long_description = soup.find_all('div', attrs={'class': 'sc-3zvnd-0 hOgsAE'})
+    product_images = soup.find_all('img', attrs={'class': 'sc-10zw1uf-11 gyQcYf'})
+    product_information = soup.find_all('div', attrs={'class': 'sc-3zvnd-0 hOgsAE'})
+    uncleaned_product_categories = soup.find_all('a', attrs={'class': 'sc-scqi9x-1 kWVUbN'})
+
+    the_regex = re.compile('^[^?]+')
+    for i in range(len(product_images)):
+        the_string = product_images[i]['src']
+        found = re.search(the_regex, the_string).group(0)
+        product_images[i] = found
+
+    the_regex = re.compile('(?<=>)[a-zA-Z0-9\ &;!,/]+(?=<)')
+    for i in range(len(product_information)):
+        the_string = str(product_information[i])
+        found = re.findall(the_regex, the_string)
+        product_information[i] = found
+    # for item in uncleaned_product_long_description:
+    #     product_long_description.append(re.findall('(?<=>)[a-zA-Z0-9\ &;!,/]+(?=<)', str(item)))
+
+    product_categories = []
+    for item in uncleaned_product_categories:
+        if item.text.strip() != 'Home':
+            product_categories.append(item.text.strip())
+    product_categories = set(product_categories)
+    product_categories = list(product_categories)
 
     # print(soup.prettify())
-    print(product_name)
-    print(product_price)
-    print(product_short_description)
-    print(product_brand)
-    print(product_main_image)
-    for item in product_more_images:
-        print(item['src'])
 
-    product_long_description = []
-    for item in uncleaned_product_long_description:
-        product_long_description.append(re.findall('(?<=>)[a-zA-Z0-9\ &;!,/]+(?=<)', str(item)))
-
-    print(product_long_description)
+    product = {
+        'product_name': product_name,
+        'product_price': product_price,
+        'product_description': product_description,
+        'product_brand': product_brand,
+        'product_images': product_images,
+        'product_information': product_information,
+        'product_categories': product_categories,
+    }
+    print(product)
 
 
 def selenium_scroll_and_return_html(url, driver):
@@ -145,5 +165,5 @@ def start_scrap():
 
 
 if __name__ == '__main__':
-    start_scrap()
-    # scrap_product_details('https://www.fairprice.com.sg/product/magiclean-bathroom-stain-mold-remover-400ml-12329292')
+    # start_scrap()
+    scrap_product_details('https://www.fairprice.com.sg/product/magiclean-bathroom-stain-mold-remover-400ml-12329292')
