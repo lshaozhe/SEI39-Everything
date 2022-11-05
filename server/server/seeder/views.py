8924a626b4c2d235c6f93df8b9dbf1e08ff0f7ab
lshaozhe <104511400+lshaozhe@users.noreply.github.com>
2022-11-06 01:49:11 +0800

    working on seeder views

--- server/server/seeder/views.py
+++ server/server/seeder/views.py
@@ -1,15 +1,23 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from . import serializer
+from . import serializers
 from . import models
 
 
 class Index(APIView):
-    def get(self, request):
+    def post(self, request):
+        print(request.data.key)
         return Response('Route works on index')
 
 
 class SeedProducts(APIView):
-    def get(self, request):
+    def put(self, request):
+        serializer = serializers.ProductsSerializer(data=request.data)
+        print(request.data)
 
-        return Response('seed route accessed')
+        if serializer.is_valid():  # required to check if serializer is correct before saving
+            serializer.save()
+            return Response(serializer.data)
+
+        else:
+            return Response(serializer.errors)
