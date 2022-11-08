206f4dbc8cdca293b2225c45d6ecf55cf2f464f1
szlam <lam_shao_zhe@defence.gov.sg>
2022-11-08 10:14:34 +0800

    added models for products

--- server_django/main_app/settings.py
+++ server_django/main_app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products'
+    # 3rd Party Apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sei39_project4',
+        'USER': 'postgres',
+        'PASSWORD': 'example',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- server_django/products/models.py
+++ server_django/products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    product_id = models.SmallAutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    product_price = models.CharField(max_length=8)
+    product_description = models.CharField(max_length=255)
+    product_brand = models.CharField(max_length=100)
+    product_stock = models.SmallIntegerField()
+    product_reserved = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.product_id
+
+
+class ProductsImages(models.Model):
+    products = models.ForeignKey('Products', on_delete=models.DO_NOTHING, related_name='products_images')
+    product_image_url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.product_image_url
+
+
+class ProductsCategories(models.Model):
+    products = models.ForeignKey('Products', on_delete=models.DO_NOTHING, related_name='products_categories')
+    product_category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.product_category
+
+
+class ProductsURLs(models.Model):
+    products = models.ForeignKey('Products', on_delete=models.DO_NOTHING, related_name='products_urls')
+    product_origin_url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.product_origin_url
+
+
+class ProductsInformation(models.Model):
+    products = models.ForeignKey('Products', on_delete=models.DO_NOTHING, related_name='products_information')
+    information_title = models.CharField(max_length=100)
+    information_details = models.TextField(blank=True)
+
+    def __str__(self):
+        return '%s: %s' % (self.product_title, self.product_details)
