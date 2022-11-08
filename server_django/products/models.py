b3e0f3986632ffb21048eb5a8697127e1548908f
szlam <lam_shao_zhe@defence.gov.sg>
2022-11-08 10:23:21 +0800

    refined models for products

--- server_django/products/models.py
+++ server_django/products/models.py
@@ -9,13 +9,14 @@ class Products(models.Model):
     product_brand = models.CharField(max_length=100)
     product_stock = models.SmallIntegerField()
     product_reserved = models.SmallIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.product_id
 
 
 class ProductsImages(models.Model):
-    products = models.ForeignKey('Products', on_delete=models.DO_NOTHING, related_name='products_images')
+    products = models.ForeignKey(Products, on_delete=models.DO_NOTHING, related_name='products_images')
     product_image_url = models.CharField(max_length=255)
 
     def __str__(self):
@@ -23,7 +24,7 @@ class ProductsImages(models.Model):
 
 
 class ProductsCategories(models.Model):
-    products = models.ForeignKey('Products', on_delete=models.DO_NOTHING, related_name='products_categories')
+    products = models.ForeignKey(Products, on_delete=models.DO_NOTHING, related_name='products_categories')
     product_category = models.CharField(max_length=100)
 
     def __str__(self):
@@ -31,7 +32,7 @@ class ProductsCategories(models.Model):
 
 
 class ProductsURLs(models.Model):
-    products = models.ForeignKey('Products', on_delete=models.DO_NOTHING, related_name='products_urls')
+    products = models.ForeignKey(Products, on_delete=models.DO_NOTHING, related_name='products_urls')
     product_origin_url = models.CharField(max_length=255)
 
     def __str__(self):
@@ -39,9 +40,18 @@ class ProductsURLs(models.Model):
 
 
 class ProductsInformation(models.Model):
-    products = models.ForeignKey('Products', on_delete=models.DO_NOTHING, related_name='products_information')
+    products = models.ForeignKey(Products, on_delete=models.DO_NOTHING, related_name='products_information')
     information_title = models.CharField(max_length=100)
     information_details = models.TextField(blank=True)
 
     def __str__(self):
         return '%s: %s' % (self.product_title, self.product_details)
+
+
+class InformationDetails(models.Model):
+    productsinformation = \
+        models.ForeignKey(ProductsInformation, on_delete=models.DO_NOTHING, related_name='information_details')
+    information_details = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.information_details
