927cce1c9120869185c06f41f750cd668a7b1270
lshaozhe <104511400+lshaozhe@users.noreply.github.com>
2022-11-08 16:48:24 +0800

    CRUD for one product for 1 table done

--- server_django/products/urls.py
+++ server_django/products/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('create/', views.CreateOneProduct.as_view(), name='create_one_product'),
     path('get/<int:pk>', views.GetOneProduct.as_view(), name='get_one_product'),
     path('getmany/', views.GetManyProducts.as_view(), name='get_many_products'),
-    path('update/<int:product_id>', views.UpdateOneProduct.as_view(), name='update_one_product'),
+    path('update/<int:pk>', views.UpdateOneProduct.as_view(), name='update_one_product'),
 ]
 
--- server_django/products/views.py
+++ server_django/products/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework import status, generics, mixins
 from . import serializers
 from . import models
-import random
 
 
 class Index(APIView):
@@ -12,61 +11,96 @@ class Index(APIView):
         return Response(data=response, status=status.HTTP_200_OK)
 
 
-# class CreateOneProduct(APIView):
-#
-#     def put(self, request):
-#         data = request.data
-#         data['product_stock'] = random.randint(1, 100)
-#         serializer = serializers.ProductsSerializer(data=data)
-#
-#         if serializer.is_valid():
-#             serializer.save()
-#             response = {'message': 'created one product',
-#                         'data': serializer.data}
-#             return Response(data=response, status=status.HTTP_201_CREATED)
-#         else:
-#             response = {'message': 'creation failed',
-#                         'data': serializer.errors}
-#             return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+class CreateOneProduct(APIView):
 
+    def put(self, request):
+        data = request.data
 
-# class GetOneProduct(APIView):
-#     serializer_class = serializers.ProductsSerializer
-#
-#     def get(self, request, pk):
-#         results = models.Products.objects.get(pk=pk)
-#
-#         serializer = self.serializer_class(instance=results)
-#         response = {'message': 'found one product',
-#                     'data': serializer.data}
-#         return Response(data=response, status=status.HTTP_200_OK)
+        serializer = serializers.ProductsSerializer(data=data)
 
-class GetOneProduct(generics.GenericAPIView, mixins.RetrieveModelMixin):
-    serializer_class = serializers.ProductsSerializer
-    queryset = models.Products.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-
-class CreateOneProduct(generics.GenericAPIView, mixins.CreateModelMixin):
-    serializer_class = serializers.ProductsSerializer
-    queryset = models.Products.objects.all()
-
-    def put(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class GetManyProducts(generics.GenericAPIView, mixins.ListModelMixin):
-    serializer_class = serializers.ProductsSerializer
-    queryset = models.Products.objects.all()
-
-    def get(self, request):
-        return self.list(request)
+        if serializer.is_valid():
+            serializer.save()
+            response = {'message': 'created one product with product_id: {}'.format(serializer.data.get('product_id')),
+                        'data': serializer.data}
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        else:
+            response = {'message': 'creation failed',
+                        'data': serializer.errors}
+            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UpdateOneProduct(APIView):
 
-    def patch(self, request, product_id):
-        print(product_id)
-        return Response('ok')
+    def patch(self, request, pk):
+        data = request.data
+        results = models.Products.objects.get(pk=pk)
+
+        serializer = serializers.ProductsSerializer(instance=results, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            response = {'message': 'updated one product with product_id: {}'.format(serializer.data.get('product_id')),
+                        'data': serializer.data}
+            return Response(data=response, status=status.HTTP_202_ACCEPTED)
+        else:
+            response = {'message': 'update failed',
+                        'data': serializer.errors}
+            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetOneProduct(APIView):
+    serializer_class = serializers.ProductsSerializer
+
+    def get(self, request, pk):
+        results = models.Products.objects.get(pk=pk)
+
+        serializer = self.serializer_class(instance=results)
+        response = {'message': 'success',
+                    'data': serializer.data}
+        return Response(data=response, status=status.HTTP_200_OK)
+
+
+class GetManyProducts(APIView):
+    serializer_class = serializers.ProductsSerializer
+
+    def get(self, request):
+        results = models.Products.objects.all()
+
+        serializer = self.serializer_class(instance=results, many=True)
+        response = {'message': 'success',
+                    'data': serializer.data}
+        return Response(data=response, status=status.HTTP_200_OK)
+
+
+
+
+# class GetOneProduct(generics.GenericAPIView, mixins.RetrieveModelMixin):
+#     serializer_class = serializers.ProductsSerializer
+#     queryset = models.Products.objects.all()
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+
+# class CreateOneProduct(generics.GenericAPIView, mixins.CreateModelMixin):
+#     serializer_class = serializers.ProductsSerializer
+#     queryset = models.Products.objects.all()
+#
+#     def put(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+# class GetManyProducts(generics.GenericAPIView, mixins.ListModelMixin):
+#     serializer_class = serializers.ProductsSerializer
+#     queryset = models.Products.objects.all()
+#
+#     def get(self, request):
+#         return self.list(request)
+
+
+# class UpdateOneProduct(generics.GenericAPIView, mixins.UpdateModelMixin):
+#     serializer_class = serializers.ProductsSerializer
+#     queryset = models.Products.objects.all()
+#
+#     def patch(self, request):
+#         return Response('ok')
