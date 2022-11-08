2d4b9b2a025784b2cac64a7c469671bc7953adf9
lshaozhe <104511400+lshaozhe@users.noreply.github.com>
2022-11-08 14:58:06 +0800

    testing views

--- server_django/products/urls.py
+++ server_django/products/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     # path('createone/', views.SeedOneProduct.as_view(), name='create_one_products'),
     # path('createmany/', views.SeedManyProducts.as_view(), name='create_many_products'),
-    path('product/create/', views.CreateOneProduct.as_view(), name='create_one_product'),
-    path('product/update/<int:product_id>', views.UpdateOneProduct.as_view(), name='update_one_product'),
+    path('create/', views.CreateOneProduct.as_view(), name='create_one_product'),
+    path('get/<int:pk>', views.GetOneProduct.as_view(), name='get_one_product'),
+    path('update/<int:product_id>', views.UpdateOneProduct.as_view(), name='update_one_product'),
 ]
 
--- server_django/products/views.py
+++ server_django/products/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from . import serializers
 from . import models
 import random
@@ -7,7 +8,8 @@ import random
 
 class Index(APIView):
     def get(self, request):
-        return Response('Route works on index')
+        response = {'message': 'api/products/ Index route is working'}
+        return Response(data=response, status=status.HTTP_200_OK)
 
 
 class CreateOneProduct(APIView):
@@ -16,12 +18,28 @@ class CreateOneProduct(APIView):
         data = request.data
         data['product_stock'] = random.randint(1, 100)
         serializer = serializers.ProductsSerializer(data=data)
-        print(serializer.is_valid)
+
         if serializer.is_valid():
-            response = {'status': 'ok', 'message': 'created'}
-            return Response(response)
+            serializer.save()
+            response = {'message': 'created one product',
+                        'data': serializer.data}
+            return Response(data=response, status=status.HTTP_201_CREATED)
         else:
-            return Response('failed')
+            response = {'message': 'creation failed',
+                        'data': serializer.errors}
+            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetOneProduct(APIView):
+    serializer_class = serializers.ProductsSerializer
+
+    def get(self, request, pk):
+        results = models.Products.objects.get(pk=pk)
+
+        serializer = self.serializer_class(instance=results)
+        response = {'message': 'found one product',
+                    'data': serializer.data}
+        return Response(data=response, status=status.HTTP_200_OK)
 
 
 class UpdateOneProduct(APIView):
