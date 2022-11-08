44e7c3c82d83ecbdcd07eaad96fad14011f59d8a
lshaozhe <104511400+lshaozhe@users.noreply.github.com>
2022-11-08 23:26:46 +0800

    removed server folder

--- server/server/seeder/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SeederConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seeder'
--- server/server/standardroutes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Index.as_view(), name='index'),
-    path('getoneproduct/<int:product_id>', views.GetOneProduct.as_view(), name='get_one_product'),
-    # path('seedmany/', views.SeedManyProducts.as_view(), name='seed_many_products'),
-]
