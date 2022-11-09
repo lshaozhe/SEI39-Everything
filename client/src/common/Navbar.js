512eef5ab36e802cb4eb857c636e03a5fb38559e
lshaozhe <104511400+lshaozhe@users.noreply.github.com>
2022-11-09 22:05:38 +0800

    do login until halfway

--- client/src/common/Navbar.js
+++ client/src/common/Navbar.js
@@ -5,49 +5,56 @@ import logo from "../assets/logo.png";
 const NavBar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink to="/main">
-          <button class="navbar-brand">
-            <img src={logo} width="30" height="30" class="d-inline-block align-top" alt="" />
-            My Shopping App
-          </button>
-        </NavLink>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a>some tab</a>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid flex">
+          <div className="col-3">
+            <NavLink to="/home">
+              <button className="navbar-brand">
+                <img
+                  src={logo}
+                  width="30"
+                  height="30"
+                  class="d-inline-block align-top"
+                  alt="LOGO"
+                />
+                My Shopping App
+              </button>
+            </NavLink>
           </div>
-          <input
-            class="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success my-2 m-sm-0" type="submit">
-            Search
-          </button>
+          <div className="col-4">
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="col-2 ms-auto">
+            <NavLink to="/login">
+              <button className="btn btn-outline-primary">
+                Login / Sign Up
+              </button>
+            </NavLink>
+          </div>
         </div>
       </nav>
     </>
-    // <NavLink
-    //   className={(navData) =>
-    //     navData.isActive
-    //       ? "block text-2xl font-bold underline decoration-4 py-2 pr-4 pl-3 rounded md:hover:text-white text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
-    //       : "block text-2xl font-bold py-2 pr-4 pl-3 rounded md:hover:text-white text-gray-400 hover:bg-gray-900 hover:text-white md:hover:bg-transparent border-gray-700 "
-    //   }
-    //   to="/watchlist">
-    //   Watchlist
-    // </NavLink>
   );
 };
 
 export default NavBar;
+
+// <NavLink
+//   className={(navData) =>
+//     navData.isActive
+//       ? "block text-2xl font-bold underline decoration-4 py-2 pr-4 pl-3 rounded md:hover:text-white text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
+//       : "block text-2xl font-bold py-2 pr-4 pl-3 rounded md:hover:text-white text-gray-400 hover:bg-gray-900 hover:text-white md:hover:bg-transparent border-gray-700 "
+//   }
+//   to="/watchlist">
+//   Watchlist
+// </NavLink>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-const login = () => {
+const Login = () => {
+  const [loginInfo, setLoginInfo] = useState({ email: "", password: "" });
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    if (e.target.id === "inputEmail") {
+      setLoginInfo({ ...loginInfo, email: e.target.value });
+    }
+    if (e.target.id === "inputPassword") {
+      setLoginInfo({ ...loginInfo, password: e.target.value });
+    }
+  };
+
+  const handleSubmit = (e) => {};
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <form>
+            <div className="mb-3">
+              <label htmlfor="inputEmail" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail"
+                aria-describedby="emailHelp"
+                value={loginInfo.email}
+                onChange={onInputChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlfor="inputPassword" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword"
+                value={loginInfo.password}
+                onChange={onInputChange}
+              />
+            </div>
+
+            <div className="mb-3 align-self-end">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+            <button type="submit" classNames="btn btn-primary">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default login;
+export default Login;
