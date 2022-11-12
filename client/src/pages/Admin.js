38648e2b759c71e726d6aabf5320d1bd25ec888a
lshaozhe <104511400+lshaozhe@users.noreply.github.com>
2022-11-13 00:59:23 +0800

    delete done on admin js

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,9 +1,36 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import ContextStorage from "../misc/context";
+import useFetch from "../misc/useFetch";
 
 const Admin = () => {
-  const { userDetails, currentSelection, setCurrentSelection } =
-    useContext(ContextStorage);
+  const {
+    context: ctxURL,
+    userDetails,
+    currentSelection,
+    setCurrentSelection,
+  } = useContext(ContextStorage);
+  const navigate = useNavigate();
+  const { response, fetchPost } = useFetch(
+    ctxURL.current.APIendpoint + "/api/products/get/" + currentSelection
+  );
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setCurrentSelection(e.target.value);
+  };
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    fetchPost(
+      ctxURL.current.APIendpoint + "/api/products/update/" + currentSelection,
+      "DELETE",
+      JSON.stringify(currentSelection)
+    );
+    setTimeout(() => {
+      navigate("/home");
+    }, 1000);
+  };
 
   const toggleAdminView = () => {
     if (userDetails.isAdmin === true) {
@@ -13,14 +40,20 @@ const Admin = () => {
             <label htmlFor="productID" className="form-label">
               Select a product ID to edit
             </label>
-            <input id="productID" className="form-control"></input>
+            <input
+              id="productID"
+              className="form-control"
+              onChange={handleInputChange}
+            ></input>
           </div>
           <br />
           <div className="row">
             <button className="btn btn-primary">Edit this Product</button>
           </div>
           <div className="row">
-            <button className="btn btn-danger">Delete this Product</button>
+            <button className="btn btn-danger" onClick={handleDelete}>
+              Delete this Product
+            </button>
           </div>
           <br />
           <div className="row">
