dd71e16c73d521a5fdad1eba798c08e9528635b7
szlam <lam_shao_zhe@defence.gov.sg>
2022-11-17 11:05:00 +0800

    updated shopping cart

--- client/src/pages/ShoppingCart.js
+++ client/src/pages/ShoppingCart.js
@@ -1,16 +1,28 @@
 import React, { useContext } from "react";
 import ContextStorage from "../misc/context";
-import Cart from "../components/Cart";
 
 const ShoppingCart = () => {
-  const { cart, setCart } = useContext(ContextStorage);
+  const { cart } = useContext(ContextStorage);
+
+  const shoppingCartMapper = () => {
+    return cart.map((element) => (
+      <div className="col-sm-12 row border">
+        <div className="col-sm-4">
+          <img src={element.product_image} style={{ height: "200px", width: "200px" }} />
+        </div>
+        <div className="col-sm-8">
+          <h3>{element.product_name}</h3>
+          <h5>QTY: {element.product_qty}</h5>
+        </div>
+      </div>
+    ));
+  };
 
   return (
-    <div className="container">
-      <Cart />
-
-      <div className="row">
-        <button> Submit my order</button>
+    <div className="container flex">
+      {shoppingCartMapper()}
+      <div className="row justify-content-center">
+        <button className="btn btn-success col-sm-3">Submit my order</button>
       </div>
     </div>
   );
