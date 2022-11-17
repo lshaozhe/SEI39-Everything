8f86e5076c1185c964da97fa07927d5ba742dfdd
szlam <lam_shao_zhe@defence.gov.sg>
2022-11-17 11:39:34 +0800

    updated shopping cart submit button

--- client/src/pages/ShoppingCart.js
+++ client/src/pages/ShoppingCart.js
@@ -1,14 +1,28 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import ContextStorage from "../misc/context";
 
 const ShoppingCart = () => {
-  const { cart } = useContext(ContextStorage);
+  const { cart, setCart } = useContext(ContextStorage);
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    setCart([]);
+
+    setTimeout(() => {
+      navigate("/home");
+    }, 1000);
+  };
 
   const shoppingCartMapper = () => {
     return cart.map((element) => (
       <div className="col-sm-12 row border">
         <div className="col-sm-4">
-          <img src={element.product_image} style={{ height: "200px", width: "200px" }} />
+          <img
+            src={element.product_image}
+            style={{ height: "200px", width: "200px" }}
+            alt="product item"
+          />
         </div>
         <div className="col-sm-8">
           <h3>{element.product_name}</h3>
@@ -22,7 +36,9 @@ const ShoppingCart = () => {
     <div className="container flex">
       {shoppingCartMapper()}
       <div className="row justify-content-center">
-        <button className="btn btn-success col-sm-3">Submit my order</button>
+        <button className="btn btn-success col-sm-3" onClick={submitHandler}>
+          Submit my order
+        </button>
       </div>
     </div>
   );
